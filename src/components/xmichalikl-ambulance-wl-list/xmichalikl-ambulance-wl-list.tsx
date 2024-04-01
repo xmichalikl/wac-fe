eb80db569f0884c7822d720e4aadf7b38d122e3a
xmichalikl <xmichalikl@stuba.sk>
2024-04-02 00:19:10 +0200

    pucek

--- src/components/xmichalikl-ambulance-wl-list/xmichalikl-ambulance-wl-list.tsx
+++ src/components/xmichalikl-ambulance-wl-list/xmichalikl-ambulance-wl-list.tsx
@@ -11,7 +11,7 @@ export class XmichaliklAmbulanceWlList {
   private async getWaitingPatientsAsync() {
     return await Promise.resolve([
       {
-        name: 'Jožko Púčik',
+        name: 'Jožko Púček',
         patientId: '10001',
         since: new Date(Date.now() - 10 * 60).toISOString(),
         estimatedStart: new Date(Date.now() + 65 * 60).toISOString(),
